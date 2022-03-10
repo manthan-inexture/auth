e1657e14c5c79b17a72af2333f6ca671506ee2d8
manthan.inexture <manthanp.inexture@gmail.com>
2022-03-10 15:34:40 +0530

    commit

--- src/component/About.js
+++ src/component/About.js
@@ -3,7 +3,7 @@ import { Outlet} from 'react-router-dom';
 const About = () => {
   return (
     <div className='text-center'><h1>About</h1>
-      <p>my name is manthan</p>
+      <p>my name is Manthan Panchal</p>
     </div>
   )
 }
